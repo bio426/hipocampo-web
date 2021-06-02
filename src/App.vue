21e03029e0d50b2dbe98acc16a86c26d249e6121
bio426 <bildla@hotmail.com>
2021-06-01 23:18:49 -0500

    header parcialmente terminado, cards para libros creadas

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
-
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Nunito', sans-serif;
+  font-family: 'Montserrat', sans-serif;
 }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="index">
     <Header />
+    <div class="container">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio autem quas modi? Quisquam voluptatem ad autem corrupti vero aspernatur, dolorum quae illo architecto voluptas deserunt provident ullam necessitatibus. A, quis? This defines the default size of an element before the remaining space is distributed. It can be a length (e.g. 20%, 5rem, etc.) or a keyword. The auto keyword means “look at my width or height property” (which was temporarily done by the main-size keyword until deprecated). The content keyword means “size it based on the item’s content” – this keyword isn’t well supported yet, so it’s hard to test and harder to know what its brethren max-content, min-content, and fit-content do.</p>
+      <h3>Libros Populares</h3>
+      <div class="wrapper">
+        <Card v-for="i in 5" :key="i" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +15,15 @@
 import {ref} from "vue"
 
 import Header from "@/components/Header.vue"
+import Card from "@/components/Card.vue"
 import Footer from "@/components/Footer.vue"
 
 export default {
+  name: "index",
   components: {
     Header,
-    Footer
+    Card,
+    Footer,
   }
 
 }
